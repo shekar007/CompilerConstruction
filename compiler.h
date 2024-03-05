825161d12bee3e218d3522e62dcb1d0ea5c0a14e
aaradhyakul <aaradhyakul@gmail.com>
2024-03-05 14:36:39 +0530

    driver.c

---
+++ compiler.h
@@ -0,0 +1,11 @@
+#ifndef COMPILER_H
+#define COMPILER_H
+#include "lexer.h"
+#include "lexerDef.h"
+#include "parser.h"
+#include "parserDef.h"
+#include "symbolTable.h"
+#include "symbolTableDef.h"
+#include "stack.h"
+#include "stackDef.h"
+#endif
---
+++ driver.c
@@ -0,0 +1,91 @@
+#include "compiler.h"
+#include <time.h>
+
+void printCommentFreeCode(FILE *codeFile)
+{
+    char *line = (char *)malloc(1024);
+    char *ptr;
+    char *printstr = (char *)malloc(1024);
+    char first_char;
+    while (fgets(line, 1024, codeFile) != NULL)
+    {
+        ptr = line;
+        printstr = line;
+        int indent_cnt = 0;
+        while (*ptr == ' ')
+        {
+            indent_cnt++;
+            ptr++;
+        }
+        if (*ptr == '%')
+        {
+            continue;
+        }
+        if (*ptr == '\n')
+        {
+            continue;
+        }
+        char *temp = printstr;
+        sscanf(ptr, "%[^%\n]", printstr);
+        char *indentstr = (char *)malloc(indent_cnt + 1);
+        if (indent_cnt != 0)
+        {
+            printf("%s%s\n", indentstr, printstr);
+        }
+        else
+        {
+            printf("%s\n", printstr);
+        }
+    }
+}
+void printTokens(symTable *sym_table)
+{
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc != 3)
+    {
+        printf("supply 2 files\n");
+        return 0;
+    }
+    FILE *codeFile = fopen(argv[1], "r");
+    FILE *parseTreeOutFile = fopen(argv[2], "rw");
+    int i;
+    while (true)
+    {
+        printf("Enter 1, 2, 3 or 0(quit)\n");
+        scanf(" %d", &i);
+
+        if (i == 0)
+        {
+            printf("exit\n");
+            break;
+        }
+        if (i == 1)
+        {
+            printCommentFreeCode(codeFile);
+        }
+        if (i == 2)
+        {
+            symTable *sym_table = callLexer(codeFile);
+            printTokens(sym_table);
+        }
+        if (i == 3)
+        {
+            callParser(codeFile, parseTreeOutFile); // which indirectly calls lexer
+        }
+        if (i == 4)
+        {
+            clock_t start_time, end_time;
+            double total_CPU_time, total_CPU_time_in_seconds;
+            start_time = clock();
+            callParser(codeFile, parseTreeOutFile);
+            end_time = clock();
+            total_CPU_time = (double)(end_time - start_time);
+            total_CPU_time_in_seconds = total_CPU_time / CLOCKS_PER_SEC;
+            printf("total_CPU_time:%lf", total_CPU_time);
+            printf("total_CPU_time_in_seconds:%lf", total_CPU_time_in_seconds);
+        }
+    }
+}
